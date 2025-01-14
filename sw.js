895b3d73665277765653961b20ae2e0e67b729ba
Jab <businessmonkey640@gmail.com>
2025-01-14 20:48:15 +0700

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const CACHE_URLS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/ico128x128.ico',
+    '/ico512x512ico'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(CACHE_URLS);
+        })
+    );
+});
+
+// Fetch requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
